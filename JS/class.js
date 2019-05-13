d6e7a56c65113149d61cea82e212823d19afc1dc
t0shi <marcindymek24@gmail.com>
2019-05-13 21:54:42 +0200

    adding fuctions in class.js

---
+++ JS/class.js
@@ -0,0 +1,10 @@
+'use strict'
+
+function switchClassName(obj, word) {
+
+ if (obj.className==this.word) {
+  return delete obj.className.word;
+ } else {
+  return obj.className.push({word});
+ }
+}
